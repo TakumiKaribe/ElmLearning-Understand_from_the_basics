47c5cafeed1543c570111615496660624ab77260
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-23 16:25:06 +0900

    no message

--- 03/17/src/Main.elm
+++ 03/17/src/Main.elm
@@ -1,6 +1,13 @@
-import Html exposing (Html, a, text)
+import Html exposing (Html, a, text, div, h1, ul, li)
 import Html.Attributes exposing (href)
 
 main : Html msg
 main =
-    a [ href "https://elm-lang.org" ] [ text "Elm" ]
+    div []
+        [ h1 [] [ text "Useful Links" ]
+        , ul []
+            [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
+            , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
+            , li [] [ a [ href "https://ellie-app.com" ] [ text "Palyground" ] ]
+            ]
+        ]
