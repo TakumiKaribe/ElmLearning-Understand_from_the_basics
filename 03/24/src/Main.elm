8ff67ee7f1e267ba91f596d7f870049f8ffe9743
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-24 13:25:05 +0900

    no message

--- 03/24/src/Main.elm
+++ 03/24/src/Main.elm
@@ -20,38 +20,61 @@ main =
 
 -- MODEL
 
-type alias Model
-    = String
+type alias Model =
+    -- タイムゾーン
+    { zone : Time.Zone
+    -- 現在時刻
+    , time : Time.Posix}
 
 init : () -> ( Model, Cmd Msg )
-init _ = ( "", Cmd.none )
+init _ =
+    -- タイムゾーンの初期値をUTCにする
+    ( Model Time.utc (Time.millisToPosix 0)
+    -- 実行環境のタイムゾーンを取得するコマンド
+    , Task.perform AdjustTimeZone Time.here
+    )
 
 
 -- UPDATE
 
 type Msg
-    = Hoge
-    | Fuga
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Hoge ->
-            ( model, Cmd.none )
+        -- 1秒おきに時刻を含んだメッセージが届く
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
 
-        Fuga ->
-            ( model, Cmd.none )
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
 
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    -- 1秒おきにTickメッセージを受け取れるようにする
+    Time.every 1000 Tick
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    -- 実行環境のタイムゾーンを考慮しながら「時：分：秒」の形にする
+    let
+        hour =
+            String.fromInt (Time.toHour model.zone model.time)
+        minute =
+            String.fromInt (Time.toMinute model.zone model.time)
+        second =
+            String.fromInt (Time.toSecond model.zone model.time)
+    in
+    h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
