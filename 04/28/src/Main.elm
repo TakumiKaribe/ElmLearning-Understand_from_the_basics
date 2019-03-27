8a320bb8f0cf9681528b817049dde7f525696741
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-28 01:59:25 +0900

    npm init and elm init

---
+++ 04/28/src/Main.elm
@@ -0,0 +1,25 @@
+import Html exposing (Html, a, text, div, h1, ul, li)
+import Html.Attributes exposing (href)
+
+main : Html msg
+main =
+    div []
+        [ header
+        , content
+        ]
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+content : Html msg
+content =
+    ul []
+        [ linkItem "https://elm-lang.org" "Homepage"
+        , linkItem "https://package.elm-lang.org" "Packages"
+        , linkItem "https://ellie-app.com" "Palyground"
+        ]
+
+linkItem : String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url ] [ text text_ ] ]
